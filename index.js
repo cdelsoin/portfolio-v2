5df53df159dbd0c1642d96b6b0d61cf6fa45d5bf
cdelsoin <cdelsoin@gmail.com>
2016-10-20 03:16:33 -0400

    update summary
    remove home from nav
    add background to default nav

--- index.js
+++ index.js
@@ -4,11 +4,6 @@
 // var example = require('./example');
 
 // load manifests
-// scripts
-require('./assets/scripts/index.js');
-
-// styles
-require('./assets/styles/index.scss');
 
 // attach jQuery globally
 require('expose?$!jquery');
